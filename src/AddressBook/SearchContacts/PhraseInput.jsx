b92cdf496da8f569651a6ebb2630b2b14d0f0901
Infinite <infiniteaengus@gmail.com>
2022-06-01 09:45:27 -0400

    Fix input issue

--- src/AddressBook/SearchContacts/PhraseInput.jsx
+++ src/AddressBook/SearchContacts/PhraseInput.jsx
@@ -1,41 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import "./PhraseInput.css";
+import './PhraseInput.css'
 
 class PhraseInput extends React.Component {
-
   static propTypes = {
     phrase: PropTypes.string.isRequired,
     onPhraseChange: PropTypes.func.isRequired,
     downshiftGetInputProps: PropTypes.func.isRequired,
-  };
-
-  componentDidMount () {
-    this.inputRef.focus();
   }
 
-  render () {
-    const {
-      phrase,
-      onPhraseChange,
-      downshiftGetInputProps,
-    } = this.props;
+  componentDidMount() {
+    this.inputRef.focus()
+  }
+
+  render() {
+    const { phrase, onPhraseChange, downshiftGetInputProps } = this.props
 
     return (
       <input
         {...downshiftGetInputProps({
-          // TODO something is wrong here
-          className: "PhraseInput",
-          ref: inputRef => this.inputRef = inputRef,
+          // TODO something is wrong here : completed
+          className: 'PhraseInput',
+          ref: (inputRef) => (this.inputRef = inputRef),
           placeholder: "To show contact's details, type its name…",
           value: phrase,
-          onChange: event => onPhraseChange("phrase"),
+          onChange: (event) => onPhraseChange(event.target.value),
         })}
       />
-    );
+    )
   }
-
 }
 
-export default PhraseInput;
+export default PhraseInput
