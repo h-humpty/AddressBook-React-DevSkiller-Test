648468f6dec95fe114a5d3e5ee78508b5677ce1e
passionatedeveloper223 <111704081+passionatedeveloper223@users.noreply.github.com>
2022-09-23 05:18:17 -0700

    adapter script

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@ import { configure } from 'enzyme';
 import EnzymeReact16Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
+// configure adapter
 configure({ adapter: new EnzymeReact16Adapter() });
