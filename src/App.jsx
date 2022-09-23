0f59ab9ab6f9970f1e91432d12468379816edbf6
passionatedeveloper223 <111704081+passionatedeveloper223@users.noreply.github.com>
2022-09-23 05:17:08 -0700

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AddressBook from "./AddressBook";
 import "./App.css";
 
 export const App = ({ reduxStore }) => (
-  <Provider store={reduxStore}>
+  <Provider store={reduxStore} className="nothing">
     <main className="App">
       <AddressBook />
     </main>
